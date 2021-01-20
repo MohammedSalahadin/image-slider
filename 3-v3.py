606e80e65c52a40d08e238f3ad28140d1e719eb8
mohammed <mohammed@me.com>
2021-01-20 17:57:16 +0300

    Rotated functinalities but some pictures are croped

--- 3-v3.py
+++ 3-v3.py
@@ -22,15 +22,15 @@ def get_image_paths(input_dir='.'):
                 paths.append(path)
     return paths
 
-def get_img_fit_size(path, scr_w, scr_h):
+def get_img_fit_size(path, scr_w, scr_h, rotate):
     
     print("Path:", path)
     imgsize = ImageTk.PhotoImage(Image.open(path))
-    
+    angle = -90
+    angle %= 360
     # Getting 
     img_w = imgsize.width()
     img_h = imgsize.height()
-    out = 0
     # when the width of the photo is more than it's height and more than screen width
     if img_w > img_h and img_w > scr_w and img_h > scr_h:
         #determining the image size accouring to the equasion
@@ -39,8 +39,15 @@ def get_img_fit_size(path, scr_w, scr_h):
         print("Case 1",img_w, img_h, scr_w, scr_h)
         var = int(img_h*(scr_w/img_w))
         img1 = Image.open(path)
-        img2 = img1.resize(((int(scr_w)-out, var-out)), Image.ANTIALIAS)
-        img = ImageTk.PhotoImage(img2)
+        img2 = img1.resize(((int(scr_w), var)), Image.ANTIALIAS)
+        
+        #checking rotation if requested or not
+        if rotate == False:
+            img = ImageTk.PhotoImage(img2)
+        elif rotate == True:
+            img = ImageTk.PhotoImage(img2.rotate(angle))
+        else:
+            print("Missing rotate attribute")
 
     
     # when the hieght of the image is more than it's width and also more than screen height
@@ -49,24 +56,53 @@ def get_img_fit_size(path, scr_w, scr_h):
         var = int(img_w*(scr_h/img_h))
         img1 = Image.open(path)
         img2 = img1.resize((var, int(scr_h)), Image.ANTIALIAS)
-        img = ImageTk.PhotoImage(img2)
+        
+        #checking rotation if requested or not
+        if rotate == False:
+            img = ImageTk.PhotoImage(img2)
+        elif rotate == True:
+            img = ImageTk.PhotoImage(img2.rotate(angle))
+        else:
+            print("Missing rotate attribute")
         
     elif img_w <= scr_w and img_h <= scr_h:
         print("Case 3")
         img1 = Image.open(path)
         img2 = img1.resize((img_w-out, img_h-out), Image.ANTIALIAS)
-        img = ImageTk.PhotoImage(img2)
+        
+        #checking rotation if requested or not
+        if rotate == False:
+            img = ImageTk.PhotoImage(img2)
+        elif rotate == True:
+            img = ImageTk.PhotoImage(img2.rotate(angle))
+        else:
+            print("Missing rotate attribute")
         
     elif img_w == img_h and img_w > scr_w:
         print("Case 4")
         img1 = Image.open(path)
         img2 = img1.resize((scr_w-out, scr_h-out), Image.ANTIALIAS)
-        img = ImageTk.PhotoImage(img2)
+        
+        #checking rotation if requested or not
+        if rotate == False:
+            img = ImageTk.PhotoImage(img2)
+        elif rotate == True:
+            img = ImageTk.PhotoImage(img2.rotate(angle))
+        else:
+            print("Missing rotate attribute")
     else:
         print("Non of above conditions",img_w, img_h, scr_w, scr_h)
         img1 = Image.open(path)
         img2 = img1.resize((img_w-out, img_h-out), Image.ANTIALIAS)
-        img = ImageTk.PhotoImage(img2)
+        
+        #checking rotation if requested or not
+        if rotate == False:
+            img = ImageTk.PhotoImage(img2)
+        elif rotate == True:
+            img = ImageTk.PhotoImage(img2.rotate(angle))
+        else:
+            print("Missing rotate attribute")
+            
     return img
 
 
@@ -128,7 +164,7 @@ def Single_view():
         
         
         
-        img = get_img_fit_size(path, scr_w, scr_h)
+        img = get_img_fit_size(path, scr_w, scr_h, False)
         my_image = canvas.create_image(int(scr_w/2),int(scr_h/2),anchor=CENTER, image=img)
         
         
@@ -161,17 +197,21 @@ def Multi_view():
         numOfImages=numOfImages+1
         path2 = paths[numOfImages]
         numOfImages=numOfImages+1
+        #Reset the while loop
         if(numOfImages>len(paths)):# if total is 5 pic, 1st loop 0 > 6 /reset the loop
-            numOfImages=0 
-    
+            numOfImages=0
+            
+            
+            
+        
         canvas = Canvas(window,width=scr_w/2, height=scr_h, bg='black')
         canvas2 = Canvas(window,width=scr_w/2, height=scr_h, bg='black')
         #gird plays the canvas without it the canvas will not work
         canvas.grid(row=0,column=0)
         canvas2.grid(row=0, column = 1)
         
-        img = get_img_fit_size(path, scr_w/2, scr_h)
-        img2 = get_img_fit_size(path2, scr_w/2, scr_h)
+        img = get_img_fit_size(path, scr_w/2, scr_h,False)
+        img2 = get_img_fit_size(path2, scr_w/2, scr_h,False)
         
         my_image = canvas.create_image(int(scr_w/4),int(scr_h/2),anchor=CENTER, image=img)
         my_image_2 = canvas2.create_image(int(scr_w/4),int(scr_h/2),anchor=CENTER, image=img2)
@@ -182,15 +222,54 @@ def Multi_view():
     window.mainloop()
     
     
+def Multi_view_rotate():
+    #directory = filedialog.askdirectory()
+    directory = r"C:\Users\DotNet\Desktop\Ragazinana Data reduced\diashow\4 Random\Portrait"
+    #Get paths
+    paths = get_image_paths(directory)
+    #read the image 
+    #call the function to get the picture object with new size
+    global numOfImages
+    
+   
+    while(numOfImages<=len(paths)-1):
+        path = paths[numOfImages]
+        numOfImages=numOfImages+1
+        path2 = paths[numOfImages]
+        numOfImages=numOfImages+1
+        if(numOfImages>len(paths)):# if total is 5 pic, 1st loop 0 > 6 /reset the loop
+            numOfImages=0 
+    
+        canvas = Canvas(window,width=scr_w/2, height=scr_h, bg='black')
+        canvas2 = Canvas(window,width=scr_w/2, height=scr_h, bg='black')
+        #gird plays the canvas without it the canvas will not work
+        canvas.grid(row=0,column=0)
+        canvas2.grid(row=0, column = 1)
+        #in order to make the picture fit in the rotated state in the half of the screen
+        # we make the get_img_fit_size adjust it to us to that size by providing 
+        # screen hight  as a width and half of the screen with as a height
+        img = get_img_fit_size(path, scr_h, scr_w/1.8, True)
+        img2 = get_img_fit_size(path2, scr_h, scr_w/1.8, True)
         
+        
+        my_image = canvas.create_image(int(scr_w/4),int(scr_h/2),anchor=CENTER, image=img)
+        
+        my_image_2 = canvas2.create_image(int(scr_w/4),int(scr_h/2),anchor=CENTER, image=img2)
+        
+        window.update()
+        time.sleep(2)
+        
+    window.mainloop()
+            
 
 
 
 button=Button(window,text="Single View",width=30,command=Single_view)
-button.place(relx=0.5, rely=0.5, anchor=CENTER)
-button2=Button(window,text="Multi View",width=30,command=Multi_view)
+button.place(relx=0.2, rely=0.5, anchor=CENTER)
+button2=Button(window,text="Multi Horezantal View",width=30,command=Multi_view)
+button2.place(relx=0.5, rely=0.5, anchor=CENTER)
+button2=Button(window,text="Multi Rotated View",width=30,command=Multi_view_rotate)
 button2.place(relx=0.7, rely=0.5, anchor=CENTER)
-   
 window.bind('<Double 1>', fullScreen)
 
 window.mainloop()
